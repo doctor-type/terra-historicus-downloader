ad85a0658beb2594ae03b136932de2857a8c2045
DoctoR-type <83175233+doctor-type@users.noreply.github.com>
2023-05-15 01:43:44 +0900

    fix to work successfully

--- download.sh
+++ download.sh
@@ -1,21 +1,21 @@
-#!/usr/bin/bash
+#!/bin/bash
 set -eu
 
 # $TH_URL="https://terra-historicus.hypergryph.com/"
 
-mkdir ./terra-historicus-downloader/
+mkdir -p ./terra-historicus-downloader/
 
-readonly INPUT=${0}
+readonly INPUT=${1}
 
 readonly PAGE_LIST_API=$(echo $INPUT | sed s#/comic#/api/comic#g)
 
-readonly PAGE_LIST_JSON=$(curl --silent $api)
+readonly PAGE_LIST_JSON=$(curl --silent $PAGE_LIST_API)
 
 readonly PAGE_COUNT=$(echo $PAGE_LIST_JSON | jq '.data.pageInfos | length')
 
-readonly PAGE_DETAIL_API=${INPUT}"/page?pageNum="
+readonly PAGE_DETAIL_API=${PAGE_LIST_API}"/page?pageNum="
 
-for $i in $(seq 1 ${PAGE_COUNT}); do
-  image_url=$(curl "$page_api$i" | jq '.data.url')
+for i in $(seq 1 ${PAGE_COUNT}); do
+  image_url=$(curl --silent "$PAGE_DETAIL_API$i" | jq --raw-output '.data.url')
   curl --silent $image_url --output ./terra-historicus-downloader/${i}.jpeg
 done
